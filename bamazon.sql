52df8c4881325f8360b47c90165505f866076e4b
Seth Randell <sethhrandell@gmail.com>
2019-08-22 23:13:12 -0400

    Added images

--- bamazon.sql
+++ bamazon.sql
@@ -5,7 +5,7 @@ CREATE DATABASE bamazon;
 USE bamazon;
 
 CREATE TABLE products (
-  id INT (4) NOT NULL,
+  id INT NOT NULL AUTO_INCREMENT,
   product_name VARCHAR(50) NOT NULL,
   department_name VARCHAR(50) NOT NULL,
   price DECIMAL(10,2) NOT NULL,
@@ -13,32 +13,32 @@ CREATE TABLE products (
   PRIMARY KEY (id)
 );
 
-INSERT INTO products (id, product_name, department_name, price, stock_quantity)
-VALUES (90,"USB Charger 24hrs", "Electronics", 25.00, 100);
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("USB Charger 24hrs", "Electronics", 25.00, 33);
 
-INSERT INTO products (id, product_name, department_name, price, stock_quantity)
-VALUES (91, "USB Charger 48hrs", "Electronics", 50.00, 100);
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("USB Charger 48hrs", "Electronics", 50.00, 25);
 
-INSERT INTO products (id, product_name, department_name, price, stock_quantity)
-VALUES (92, "Small Bluetooth Speaker", "Electronics", 75.00, 100);
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Small Bluetooth Speaker", "Electronics", 75.00, 24);
 
-INSERT INTO products (id, product_name, department_name, price, stock_quantity)
-VALUES (93, "Large Bluetooth Speaker", "Electronics", 150.00, 100);
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Large Bluetooth Speaker", "Electronics", 150.00, 48);
 
-INSERT INTO products (id, product_name, department_name, price, stock_quantity)
-VALUES (94, "Wireless Charger", "Electronics", 15.00, 100);
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Wireless Charger", "Electronics", 15.00, 58);
 
-INSERT INTO products (id, product_name, department_name, price, stock_quantity)
-VALUES (95, "iPhone X Cell Phone Case", "Accessories", 10.00, 100);
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("iPhone X Cell Phone Case", "Accessories", 10.00, 4);
 
-INSERT INTO products (id, product_name, department_name, price, stock_quantity)
-VALUES (96, "iPhone USB Cable", "Accessories", 5.00, 100);
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("iPhone USB Cable", "Accessories", 5.00, 2);
 
-INSERT INTO products (id, product_name, department_name, price, stock_quantity)
-VALUES (97, "Micro USB USB Cable", "Accessories", 5.00, 100);
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Micro USB USB Cable", "Accessories", 5.00, 3);
 
-INSERT INTO products (id, product_name, department_name, price, stock_quantity)
-VALUES (98, "iPhone X Screen Protector", "Accessories", 12.00, 100);
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("iPhone X Screen Protector", "Accessories", 12.00, 77);
 
-INSERT INTO products (id, product_name, department_name, price, stock_quantity)
-VALUES (99, "S Series Screen Protector", "Accessories", 10.00, 100);
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("S Series Screen Protector", "Accessories", 10.00, 6);
